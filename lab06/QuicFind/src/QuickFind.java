87f5c93e5cdb1648989b8b6c9f6b2793ca7abc86
teamtee <2624071330@qq.com>
2023-08-21 09:18:15 +0800

    Change lab06

--- lab06/QuicFind/src/QuickFind.java
+++ lab06/QuicFind/src/QuickFind.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class QuickFind {
+public class QuickFind {
+    private int[] items;
+    public QuickFind(int n){
+        items = new int[n];
+    }
+    public void validate(int v1){
+        if(v1 > items.length){
+            throw new IndexOutOfBoundsException();
+        }
+    }
+    public int sizeOf(int v1){
+        int setValue = items[v1];
+        int count = 1;
+        for(int x: items){
+            if( x == setValue){
+                count++;
+            }
+        }
+        return  count;
+    }
+    public boolean isConnected(int v1,int v2){
+        return items[v1] == items[v2];
+    }
+    public void  connect(int v1,int v2){
+        int setValue = items[v1];
+        for(int x = 0 ; x < items.length;x++){
+            if(items[x] == items[v2]){
+                items[x] = setValue;
+            }
+        }
+    }
 }
--- lab06/QuicFind/src/QuickUnion.java
+++ lab06/QuicFind/src/QuickUnion.java
@@ -1,2 +1,60 @@
-public class QuicUnion {
+import java.util.ArrayList;
+
+public class QuickUnion {
+    private int[] items;
+    public QuickUnion(int n){
+        items = new int[n];
+    }
+    public void validate(int v1){
+        if(v1 > items.length){
+            throw new IndexOutOfBoundsException();
+        }
+    }
+    public int parents(int v1){
+        return items[v1];
+    }
+    public int sizeOf(int v1){
+        int size = 0;
+        int root = find(v1);
+        for (int x: items){
+            if( root == find(x)){
+                size++;
+            }
+        }
+        return size;
+    }
+    public boolean isConnected(int v1,int v2){
+        return find(v1) == find(v2);
+    }
+    public void  union(int v1,int v2){
+        if(sizeOf(v1) >= sizeOf(v2)){
+            connect(find(v1),find(v2));
+            return;
+        }
+        connect(find(v2),find(v1));
+        return;
+    }
+    private void connect(int v1,int v2){
+        if (items[v1] < 0){
+            if (items[v2] < 0){
+                items[v1] = items[v1] + items[v2];
+            }
+            else {
+                items[v1] = items[v1] - 1;
+            }
+            items[v2] = v1;
+        }
+    }
+    public int find(int v1){
+        int node = v1;
+        ArrayList<Integer> path = new ArrayList<>();
+        while (items[node] >= 0 && items[node] != node){
+            path.add(node);
+            node = items[node];
+        }
+        for(int x: path){
+            items[x] = node;
+        }
+        return node;
+    }
 }
